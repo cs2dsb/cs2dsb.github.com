d4c31ad0b5308afe265fbd38260126b0efdd1ac7
benjamind <daniel.benjamin@vizolution.co.uk>
2014-09-10 14:55:42 +0100

    timesheet tool iteration

--- TimesheetTool.js
+++ TimesheetTool.js
@@ -1,1 +1,11 @@
-alert('it worked')
+(function() {
+  var script = document.createElement('script'); 
+  script.type = 'text/javascript'; 
+  script.src = 'https://code.jquery.com/jquery-2.1.1.min.js';
+  script.onreadystatechange = jqueryLoaded;
+  script.onload = jqueryLoaded;
+
+  function jqueryLoaded() {
+    console.log($);
+  }
+})();
