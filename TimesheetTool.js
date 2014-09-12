d9e47b5770145143b20a9424220bb8a7f128240c
benjamind <daniel.benjamin@vizolution.co.uk>
2014-09-12 16:11:28 +0100

    fd

--- TimesheetTool.js
+++ TimesheetTool.js
@@ -578,7 +578,7 @@
             }
             var diff = minsBetweenIntTimes(o.startTime, o.endTime);
             totalMins[o.day].time = totalMins[o.day].time + minsIntTime(diff) + hoursIntTime(diff) * 60;
-            totalMins[o.day].tasks.push(o.startTime + ', ' + o.endTime + ', ' diff);
+            totalMins[o.day].tasks.push(o.startTime + ', ' + o.endTime + ', ' + diff);
           } 
         }
 
